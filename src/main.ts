90f98a026cadc800f7585bb1c4334ec1c22175e6
Cameron Coleman <142551249+TheSkyIsForward2@users.noreply.github.com>
2025-10-19 18:08:36 +0000

    Step 7 Complete - Changed cursor hover on canvas to a small dot

--- src/main.ts
+++ src/main.ts
@@ -53,49 +53,96 @@ class MarkerLine {
   }
 }
 
+class ToolPreview {
+  x: number;
+  y: number;
+  thickness: number;
+
+  constructor(x: number, y: number, thickness: number) {
+    this.x = x;
+    this.y = y;
+    this.thickness = thickness;
+  }
+
+  updatePosition(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  updateThickness(thickness: number) {
+    this.thickness = thickness;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "rgb(0,0,0)";
+    ctx.arc(this.x, this.y, this.thickness / 2, 0, Math.PI * 2);
+    ctx.stroke();
+  }
+}
+
 const lines: MarkerLine[] = [];
 const redoLines: MarkerLine[] = [];
 
 let currentLine: MarkerLine | null = null;
-
-const cursor = { active: false, x: 0, y: 0 };
+let toolPreview: ToolPreview | null = null;
+let mouseDown = false;
 
 canvas.addEventListener("mousedown", (e) => {
-  cursor.active = true;
-  cursor.x = e.offsetX;
-  cursor.y = e.offsetY;
-
-  currentLine = new MarkerLine(cursor.x, cursor.y, currentThickness);
+  mouseDown = true;
+  const start = { x: e.offsetX, y: e.offsetY };
+  currentLine = new MarkerLine(start.x, start.y, currentThickness);
   lines.push(currentLine);
   redoLines.splice(0, redoLines.length);
-
   redraw();
 });
 
 canvas.addEventListener("mousemove", (e) => {
-  if (!cursor.active || !currentLine) return;
+  const { offsetX, offsetY } = e;
 
-  cursor.x = e.offsetX;
-  cursor.y = e.offsetY;
-  currentLine.drag(cursor.x, cursor.y);
+  // Update tool preview position & size
+  if (!toolPreview) {
+    toolPreview = new ToolPreview(offsetX, offsetY, currentThickness);
+  } else {
+    toolPreview.updatePosition(offsetX, offsetY);
+    toolPreview.updateThickness(currentThickness);
+  }
+
+  if (mouseDown && currentLine) {
+    currentLine.drag(offsetX, offsetY);
+  }
 
   redraw();
 });
 
 canvas.addEventListener("mouseup", () => {
-  cursor.active = false;
+  mouseDown = false;
   currentLine = null;
+  redraw();
+});
 
+canvas.addEventListener("mouseleave", () => {
+  toolPreview = null;
+  redraw();
+});
+
+canvas.addEventListener("mouseenter", (e) => {
+  toolPreview = new ToolPreview(e.offsetX, e.offsetY, currentThickness);
   redraw();
 });
 
 // Rendering
 function redraw() {
-  if (ctx != null) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (const line of lines) {
-      line.display(ctx);
-    }
+  ctx!.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (const line of lines) {
+    line.display(ctx!);
+  }
+
+  // Only draw the preview if mouse is not pressed
+  if (!mouseDown && toolPreview) {
+    toolPreview.draw(ctx!);
   }
 }
 
@@ -123,6 +170,8 @@ function selectTool(thickness: number, button: HTMLElement) {
   thinButton.classList.remove("selectedTool");
   thickButton.classList.remove("selectedTool");
   button.classList.add("selectedTool");
+  if (toolPreview) toolPreview.updateThickness(thickness);
+  redraw();
 }
 
 thinButton.addEventListener("click", () => selectTool(2, thinButton));
